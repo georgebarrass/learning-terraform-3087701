496d83e25e607dc49667dfda073933daecbb32e9
georgebarrass <48869859+georgebarrass@users.noreply.github.com>
2023-04-06 15:26:39 +0100

    0000

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ module "blog_sg" {
   version = "4.17.1"
   name = "blog"
   
-  vpc_id = module.vpc.vpc_id
+  vpc_id = module.vpc.vpc_id[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
